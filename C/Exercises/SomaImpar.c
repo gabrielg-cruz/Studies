28af5ae6a51fde2944828efaa9a09e05e965e6ea
Gabriel <132217603+gabrielg-cruz@users.noreply.github.com>
2023-12-16 17:05:54 -0300

    Add files via upload

---
+++ C/Exercises/SomaImpar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+	int x, y, soma;
+	int i = y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	
+	for(i; i < x; i++){
+		
+		soma = i + (y + 2);
+		printf("%d", soma);
+	}
+	
+	return 0;
+}
