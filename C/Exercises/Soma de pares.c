c7279050d1c14086b611ba03f7f38f45152056cd
Gabriel <132217603+gabrielg-cruz@users.noreply.github.com>
2024-01-31 13:28:59 -0300

    Add files via upload

---
+++ C/Exercises/Soma de pares.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, i, compar, soma = 0;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    if (x > y)
+        compar = 1;
+    else if (x < y)
+        compar = 2;
+
+    if (x == y)
+        printf("%d", soma);
+    else {
+        switch (compar) {
+            case 1:
+                for (i = x - 1; i > y; i--) {
+                    if (i % 2 != 0) {
+                        soma += i;
+                    }
+                }
+                break; 
+            case 2:
+                for (i = x + 1; i < y; i++) {
+                    if (i % 2 != 0) {
+                        soma += i;
+                    }
+                }
+                break; 
+        }
+    }
+
+    printf("%d\n", soma);
+
+    return 0;
+}
+
